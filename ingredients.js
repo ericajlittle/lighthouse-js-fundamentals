3d6cd5a6465cfbd797b33718efae9564ff98768a
Erica Little <ericalittle123@gmail.com>
2016-12-03 00:43:01 +0000

    adds problems exercise

---
+++ ingredients.js
@@ -0,0 +1,16 @@
+var ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "chocolate chips", "bananas"];
+var list = 0
+while (list < ingredients.length) {
+  list = list + 1;
+}
+console.log(ingredients);
+
+var ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "chocolate chips", "bananas"];
+for (var i = 0; i < ingredients.length; i++) {
+  console.log(ingredients[i]);
+}
+
+var ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "chocolate chips", "bananas"];
+for (var i = ingredients.length - 1; i >=0; i--) {
+  console.log(ingredients[i]);
+}
